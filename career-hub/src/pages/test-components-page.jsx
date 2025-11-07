322e9f5617b5751ed928ea229425c7ae008d0f6b
Daniel Lisko <58869028+djlisko01@users.noreply.github.com>
2025-11-07 22:19:35 +0000

    Add text input atom

--- career-hub/src/pages/test-components-page.jsx
+++ career-hub/src/pages/test-components-page.jsx
@@ -1,13 +1,14 @@
 import Button from "../components/atoms/Button/button.atom";
+import TextInput from "../components/atoms/TextInput/textinput.atom";
 
 const TestPage = () => {
-    console.log("Rendering Test Components Page")
     return (
         <>
             <h1>Test Components Page</h1>
             <Button variant="outline"> Click Me </Button>
+            <TextInput size="small"/>
         </>
-    )
+    );
 }
 
 export default TestPage;
