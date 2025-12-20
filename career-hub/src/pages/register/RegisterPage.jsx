7e7220a0b7a3fe391322d53615e71a703e5016a3
Daniel Lisko <djlisko01@gmail.com>
2025-12-19 23:39:02 -0500

    Add signup service

--- career-hub/src/pages/register/RegisterPage.jsx
+++ career-hub/src/pages/register/RegisterPage.jsx
@@ -2,10 +2,19 @@ import { Link } from "react-router";
 import AuthTemplate from "../../components/templates/AuthTemplate/auth-template";
 import RegisterForm from "../../components/organisms/Forms/RegisterForm";
 
+import AuthService from "../../services/auth.services";
+
 const RegisterPage = () => {
-    const handleRegister = (data) => {
+    const handleRegister = async (data) => {
         console.log("Register data:", data);
         // Add your registration logic here
+        const { username, password, email } = data;
+        try {
+            const newUser = await AuthService.signUp(email, password);
+            console.log("Registered user:", newUser);
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     return (
--- career-hub/src/services/auth.services.js
+++ career-hub/src/services/auth.services.js
@@ -3,8 +3,13 @@ import ApiClient from "../utils/api-client";
 const AuthService = {
     apiClient: new ApiClient("http://localhost:8000"),
     login: async function (username, password) {
-        const data = { username: username, password: password }
+        const data = { username: username, password: password };
         return await this.apiClient.post("/api/v1/auth/login", data);
+    },
+
+    signUp: async function (email, password) {
+        const data = { email: email, password: password };
+        return await this.apiClient.post("/api/v1/auth/signup", data);
     }
 }
 
