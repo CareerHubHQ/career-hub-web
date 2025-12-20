19bb9fa7e288cff6f264ad050ce8f7ed602ad701
Daniel Lisko <djlisko01@gmail.com>
2025-12-19 23:37:16 -0500

    Add scss to form fields

--- career-hub/src/components/molecules/FormField/form-field.jsx
+++ career-hub/src/components/molecules/FormField/form-field.jsx
@@ -1,12 +1,24 @@
 import Label from "../../atoms/Label/label";
 import Input from "../../atoms/Input/input.atom";
+import PropTypes from "prop-types";
+import "./form-field.scss";
 
-const FormField = ({ label, htmlFor, ...props }) => {
+const FormField = ({ label, htmlFor, errorText, helperText, ...props }) => {
     return (
-        <div className="form-field">
+        <div className={`form-field ${errorText ? "has-error" : ""}`}>
             <Label htmlFor={htmlFor}>{label}</Label>
             <Input id={htmlFor} {...props} />
+            <span className="input-subtext">{errorText || helperText}</span>
         </div>
     );
 };
+
+FormField.propTypes = {
+    label: PropTypes.string.isRequired,
+    htmlFor: PropTypes.string.isRequired,
+    errorText: PropTypes.string,
+    helperText: PropTypes.string,
+    showError: PropTypes.bool,
+}
+
 export default FormField;
